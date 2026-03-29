f678ccf8f3cf8c39a95934ed6734ac5bccc8cd5b
Jayce <maihoang171@gmail.com>
2026-03-29 16:19:37 +0700

    feat and unit test: delete and update

--- src/services/task-service.ts
+++ src/services/task-service.ts
@@ -1,7 +1,10 @@
 import prisma from "../lib/prisma.js";
-import type { CreateTaskInput } from "../schemas/task-schema.js";
+import type {
+  createTaskInput,
+  updateTaskInput,
+} from "../schemas/task-schema.js";
 
-export const createTaskService = async (data: CreateTaskInput) => {
+export const createTaskService = async (data: createTaskInput) => {
   const task = await prisma.task.create({
     data: {
       title: data.title,
@@ -20,6 +23,9 @@ export const createTaskService = async (data: CreateTaskInput) => {
 
 export const getTasksService = async () => {
   return await prisma.task.findMany({
+    where: {
+      deletedAt: null,
+    },
     select: {
       id: true,
       title: true,
@@ -29,5 +35,39 @@ export const getTasksService = async () => {
       createdAt: true,
       updatedAt: true,
     },
+  });
+};
+
+export const updateTaskService = async (
+  id: string,
+  updateTask: updateTaskInput,
+) => {
+  const cleanData = Object.fromEntries(
+    Object.entries(updateTask).filter(([_, v]) => v !== undefined),
+  );
+
+  return await prisma.task.update({
+    where: { id },
+    data: cleanData,
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      status: true,
+      deadlineAt: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+};
+
+export const deleteTaskService = (id: string) => {
+  return prisma.task.update({
+    where: { id },
+    data: { deletedAt: new Date() },
+    select:{
+      id: true,
+      title: true,
+    }
   });
 };
